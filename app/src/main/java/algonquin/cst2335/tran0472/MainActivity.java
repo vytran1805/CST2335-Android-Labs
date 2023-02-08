e952be5cf45a1f49ac4403b962331f8d5cb606b9
Vy Tran <vytran1805@gmail.com>
2023-02-08 00:52:35 -0500

    Lab 4

--- app/src/main/java/algonquin/cst2335/tran0472/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/tran0472/MainActivity.java
@@ -2,13 +2,80 @@ package algonquin.cst2335.tran0472;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import algonquin.cst2335.tran0472.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity";
 
-    @Override
+    @Override   //1) When run the program, this starts first
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+//    The first parameter is a String called the TAG. This should be the class name, and the second
+//    parameter is the message
+        Log.w( TAG, "In onCreate() - Loading Widgets" );
+
+        // save the MyData file to save login email address so that the next time they run the application, the email address can be pre-filled
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE); //create a SharedPreferences obj
+        String emailAddress = prefs.getString("LoginName","");  //first time load the app, it will be pre-filled with an empty string
+        // create Editor obj that stores email table on the disk
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("LoginName",emailAddress); //save the input to MyData file that we have opened above
+
+        variableBinding.emailEditText.setText(emailAddress);    //set the text of EditText to be the emailAddress variable
+        // float hi = prefs.getFloat("Hi",0);   //get value of name "Hi" in MyData.xml file (/data/data/cst2335.tran0472/shared_prefs)
+        // int num = prefs.getInt("Age",0); //if name is not found then return 0
+        editor.apply();
+
+//        Set on click listener for the login button to go from MainActivity page to SecondActivity page
+        variableBinding.button.setOnClickListener(btn->{
+            //Intent is "where you want to go next?" function
+            Intent nextPage = new Intent(MainActivity.this,SecondActivity.class);   //in the parenthesis is: 1) where we are now (MainActivity), 2) Which activity you want to go next (SeconActivity)
+            // this function like a spreadsheet (populate table)
+            String email = variableBinding.emailEditText.getText().toString();
+            nextPage.putExtra("EmailAddress",email);
+            // Actually makes the transition
+            startActivity(nextPage);
+        });
+
     }
+
+    @Override   // 2) The app is now visible
+    protected void onStart() {
+        super.onStart();
+        Log.w( TAG, "In onStart() - The application is now visible on screen" );
+    }
+
+    @Override   // 3) now responds to user input
+    protected void onResume() {
+        super.onResume();
+        Log.w( TAG, "In onResume() - The application is now responding to user input" );
+    }
+
+    @Override   // Opposite to onResume(), stop responding to user input
+    protected void onPause() {
+        super.onPause();
+        Log.w( TAG, "In onPause() - The application no longer responds to user input" );
+    }
+
+    @Override   // Opposite to onStart(), no longer visible
+    protected void onStop() {
+        super.onStop();
+        Log.w( TAG, "In onStop() - The application is no longer visible" );
+    }
+
+    @Override   // Opposite to onCreate(), memory is garbage collected
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( TAG, "In onDestroy() - Any memory used by the application is freed" );
+    }
+
 }
---
+++ app/src/main/java/algonquin/cst2335/tran0472/SecondActivity.java
@@ -0,0 +1,121 @@
+package algonquin.cst2335.tran0472;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Picture;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import algonquin.cst2335.tran0472.databinding.ActivityMainBinding;
+import algonquin.cst2335.tran0472.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+    ActivitySecondBinding secondBinding = ActivitySecondBinding.inflate(getLayoutInflater());
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        //ActivitySecondBinding
+
+
+        setContentView(secondBinding.getRoot());
+
+        //How to get the table
+        Intent dataFromPage1 = getIntent();
+        String emailAddress = dataFromPage1.getStringExtra("EmailAddress");
+        //get the Welcome string from strings.xml + input emailAddress then set to the textView to display
+        secondBinding.textView.setText(getString(R.string.welcome,emailAddress));   //display the email to the Second page, format "Welcome, +address"
+        Log.d("Second activity","Email Address is "+ emailAddress);
+
+        //make a phone call
+        secondBinding.secondPageButton1.setOnClickListener(btn->{
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            String phoneNumber = secondBinding.editTextPhone.getText().toString();
+            //the phone needs to know which number to call. For that, your store the phone number in the Intent
+            call.setData(Uri.parse("tel: "+phoneNumber));
+            startActivity(call);    //call the number
+        });
+
+        //  check if the picture existed, then load it immediately on screen
+        File sandbox = new File(getFilesDir(), "Picture.png");   //points to your sandbox: /data/data/algonquin.cst2335.tran0472
+        if(sandbox.exists()){
+            Bitmap theImage = BitmapFactory.decodeFile(sandbox.getAbsolutePath().toString());
+            secondBinding.imageView.setImageBitmap(theImage);
+        }
+        // take a picture with the phone
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                                                //Parameters: 1) pass in the function call above is called an ActivityResultContract:
+        //get data back from the next Activity (get picture)                  2) pass in is a callback function to receive data from the next activity
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override   //this function means when the camera has disappeared,
+            // it calls onActivityResult(), passes an intent
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {     //If the result is RESULT_OK, then the camera activity sends back a Bitmap in the Intent data object
+                    //our picture is stored in here: data
+                    Intent data = result.getData();     //intent is like a table: variable here is "data" and value is our picture (BITMAP)
+                    Bitmap thumbnail = data.getParcelableExtra("data"); //the picture is saved as a Bitmap object
+                    //save the bitmap object to the disk using the code
+
+                    FileOutputStream fOut = null;
+
+                    //this below automatically opens a file in sandbox
+                    try {
+                        fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                        thumbnail.compress(Bitmap.CompressFormat.PNG,100,fOut);    //compresses png into your sandbox
+                        fOut.flush();
+                        fOut.close();
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    secondBinding.imageView.setImageBitmap(thumbnail);  //take the returned Bitmap object and set that to be the src parameter of the ImageView
+                }
+            }
+        });
+        //display the picture once the button is clicked
+        secondBinding.secondPageButton2.setOnClickListener(btn->{
+            cameraResult.launch(cameraIntent);
+        });
+
+        //go back to the previous page (MainActivity)
+        secondBinding.goBack.setOnClickListener(btn->{
+            //send data to the previous page
+            //Intent dataTable = new Intent();    //no need from/to cause there is only 1 direction to go back
+            finish();   //return onPause(), onStop() of this page
+        });
+
+
+
+    }
+
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        SharedPreferences prefs = getSharedPreferences("MyData",Context.MODE_PRIVATE);
+//        SharedPreferences.Editor myEdit = prefs.edit();
+//        String phoneNumber = secondBinding.editTextPhone.getText().toString();
+//        myEdit.putString("PhoneNumber",phoneNumber);
+//        myEdit.apply();
+//    }
+}
